a9dee151790578b972937a9a7c1cf2b0e5cefde9
tomas <tomasz_kondej@o2.pl>
2021-01-28 23:44:34 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PalindromApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the word you want to check if it's Palindrom");
+            string input_word = Console.ReadLine();
+            Palindrom pal = new Palindrom(input_word);
+            Console.WriteLine(pal.IsPalindrom() ? "Given word/phrase is a Palindrom" : "Given word/phrase is not a Palindrom");
+
+        }
+    }
+}
