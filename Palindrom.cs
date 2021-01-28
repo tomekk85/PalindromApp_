1a1e3cd529d1acbda988c0fb007cd47b8e2360c6
tomas <tomasz_kondej@o2.pl>
2021-01-28 23:44:34 +0100

    Dodaj pliki projektów.

---
+++ Palindrom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PalindromApp
+{
+    public class Palindrom
+    {
+        private String word;
+
+        public Palindrom(string word) 
+        {
+            this.word = word;
+        }
+        public Boolean IsPalindrom() 
+        {
+            string input_ = Regex.Replace(this.word, @"\s", "");
+            input_ = input_.ToLower();
+            char[] charArray = input_.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed_ = new string(charArray);
+
+            return input_.Equals(reversed_);
+        }
+    }
+}
